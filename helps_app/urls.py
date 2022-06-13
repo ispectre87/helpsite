30eb3e0d728ab9527994deba3baa56aa17957fbe
ispectre <ispectremrpl@gmail.com>
2022-06-13 21:15:42 +0300

    Add class based views (except helprequest form)

--- helps_app/urls.py
+++ helps_app/urls.py
@@ -4,10 +4,10 @@ from django.urls import path
 app_name = 'helps_app'
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<int:region_id>/', views.cities, name='cities'),
-    path('<int:region_id>/<int:city_id>/', views.help_requests, name='help_requests'),
-    path('help_request_detail/<int:help_id>/', views.help_request_detail, name='help_request_detail'),
+    path('', views.HelpHomepage.as_view(), name='index'),
+    path('<int:region_id>/', views.CitiesList.as_view(), name='cities'),
+    path('<int:region_id>/<int:city_id>/', views.HelpRequests.as_view(), name='help_requests'),
+    path('help_request_detail/<int:help_id>/', views.HelpRequestDetail.as_view(), name='help_request_detail'),
     path('need_help/<int:city_id>/', views.need_help, name='need_help'),
     path('request_added/', views.request_added, name='request_added'),
 
--- helps_app/views.py
+++ helps_app/views.py
@@ -1,32 +1,65 @@
 from django.shortcuts import render, redirect
 from .models import HelpRequest, City, Region
 from .forms import CreateRequest
+from django.views import generic
 
-def index(request):
-    """Главная страница сайта"""
-    regions = Region.objects.all()
-    context = {
-        'regions': regions,
-    }
-    return render(request, 'helps_app/index.html', context)
+class HelpHomepage(generic.ListView):
+    model = Region                          #модель на основании которой строится список
+    template_name = 'helps_app/index.html'  #имя шаблона
+    context_object_name = 'regions'         #имя списка в самом шаблоне
 
-def cities(request, region_id):
-    """Отображает список городов по региону"""
-    cities = City.objects.filter(region_id=region_id)
-    context = {
-        'cities': cities,
-        'region_id': region_id,
-    }
-    return render(request, 'helps_app/cities.html', context)
+# def index(request):
+#     """Главная страница сайта"""
+#     regions = Region.objects.all()
+#     context = {
+#         'regions': regions,
+#     }
+#     return render(request, 'helps_app/index.html', context)
 
-def help_requests(request, city_id, region_id):
-    """Отображает список заявок по городу"""
-    help_title = HelpRequest.objects.filter(citi_name_id=city_id)
-    context = {
-        'help_title': help_title,
-        'city_id': city_id,
-    }
-    return render(request, 'helps_app/help_requests.html', context)
+class CitiesList(generic.ListView):
+    model = City
+    template_name = 'helps_app/cities.html'
+    context_object_name = 'cities'
+
+    def get_queryset(self):
+        return City.objects.filter(region_id=self.kwargs['region_id'])
+
+# def cities(request, region_id):
+#     """Отображает список городов по региону"""
+#     cities = City.objects.filter(region_id=region_id)
+#     context = {
+#         'cities': cities,
+#         'region_id': region_id,
+#     }
+#     return render(request, 'helps_app/cities.html', context)
+
+class HelpRequests(generic.ListView):
+    model = HelpRequest
+    template_name = 'helps_app/help_requests.html'
+    context_object_name = 'help_title'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data()
+        context['city_id'] = self.kwargs['city_id']
+        return context
+
+    def get_queryset(self):
+        return HelpRequest.objects.filter(citi_name_id = self.kwargs['city_id'])
+
+# def help_requests(request, city_id, region_id):
+#     """Отображает список заявок по городу"""
+#     help_title = HelpRequest.objects.filter(citi_name_id=city_id)
+#     context = {
+#         'help_title': help_title,
+#         'city_id': city_id,
+#     }
+#     return render(request, 'helps_app/help_requests.html', context)
+
+# class CreateHelpRequest(generic.CreateView):
+"""Не знаю как сделать привязку к городу"""
+#     form_class = CreateRequest
+#     template_name = 'helps_app/need_help.html'
+#     pk_url_kwarg = 'city_id'
 
 def need_help(request, city_id):
     """Форма заявки на помощь"""
@@ -42,20 +75,33 @@ def need_help(request, city_id):
     context = {'form': form, 'city_id': city_id}
     return render(request, 'helps_app/need_help.html', context=context)
 
-def help_request_detail(request, help_id):
-    """Полная информация по заявке"""
-    help_detail = HelpRequest.objects.get(pk=help_id)
-    title = help_detail.title
-    text = help_detail.text
-    date = help_detail.pub_date
-    contacts = help_detail.contacts
-    context = {
-        'title': title,
-        'text': text,
-        'date': date,
-        'contacts': contacts,
-        }
-    return render(request, 'helps_app/help_request_detail.html', context)
+class HelpRequestDetail(generic.DetailView):
+    model = HelpRequest
+    template_name = 'helps_app/help_request_detail.html'
+    context_object_name = 'request_detail'
+    pk_url_kwarg = 'help_id'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+    def get_queryset(self):
+        return HelpRequest.objects.filter(pk=self.kwargs['help_id'])
+
+# def help_request_detail(request, help_id):
+#     """Полная информация по заявке"""
+#     help_detail = HelpRequest.objects.get(pk=help_id)
+#     title = help_detail.title
+#     text = help_detail.text
+#     date = help_detail.pub_date
+#     contacts = help_detail.contacts
+#     context = {
+#         'title': title,
+#         'text': text,
+#         'date': date,
+#         'contacts': contacts,
+#         }
+#     return render(request, 'helps_app/help_request_detail.html', context)
 
 def request_added(request):
     return render(request, 'helps_app/request_added.html')
