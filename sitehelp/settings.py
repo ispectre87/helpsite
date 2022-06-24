f4709e0a913c8d2a24d1f4ddf1a182545f4374ae
ispectre <ispectremrpl@gmail.com>
2022-06-24 16:49:40 +0300

    Remove Heroku settings() from settings.py, because was a problen with collectstatic during build.

--- sitehelp/settings.py
+++ sitehelp/settings.py
@@ -28,8 +28,8 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 # Настройки Heroku
-import django_heroku
-django_heroku.settings(locals())
+# import django_heroku
+# django_heroku.settings(locals())
 
 
 # Application definition
@@ -125,7 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'   #префикс для url статических файлов
+STATIC_URL = '/static/'   #префикс для url статических файлов
 STATIC_ROOT = os.path.join(BASE_DIR, 'static') #путь к общей папке статик в которой будут собираться файлы во время работы на сервере
 STATIC_DIRS = [] #Список нестандартных путей к статическим файлам
 
