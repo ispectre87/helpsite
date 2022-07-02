f431b8cb46b9e11ec95d83b9224ca0cbde9218b8
ispectre <ispectremrpl@gmail.com>
2022-07-02 21:42:25 +0300

    Add os.environ.get('DEBUG') to settings.py4

--- sitehelp/settings.py
+++ sitehelp/settings.py
@@ -152,4 +152,12 @@ CACHES = {
 import django_heroku
 django_heroku.settings(locals())
 
+if os.environ.get('DEBUG') == 'TRUE':
+    config['DEBUG'] = True
+elif os.environ.get('DEBUG') == 'FALSE':
+    config['DEBUG'] = False
+
+
+
+
 
